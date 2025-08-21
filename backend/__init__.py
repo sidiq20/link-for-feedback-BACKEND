5f8c5a0431c522aef53f29c09854d5b0549442d9
sidiq20 <sidiqolasode@gmail.com>
2025-08-21 12:16:19 +0100

    update

--- backend/__init__.py
+++ backend/__init__.py
@@ -18,7 +18,7 @@ mongo = PyMongo()
 
 def create_app():
     app = Flask(__name__)
-    CORS(app, origins=['http://localhost:5173', 'http://127.0.0.1:5173', 'https://whisper-opal.vercel.app/'])
+    CORS(app, resources={r"/*": {"origins": ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://whisper-opal.vercel.app/']}}, supports_credentials=True)
 
     app.config.from_object(Config)
 
